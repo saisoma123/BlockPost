5051723715249eb47482710a439354295b370214
saisoma123 <suryasai.somayyajula@gmail.com>
2024-06-25 13:21:15 -0500

    Added handler logic for storing messages with keeper method

--- x/blockpost/keeper/msg_server.go
+++ x/blockpost/keeper/msg_server.go
@@ -3,6 +3,9 @@ package keeper
 import (
 	"context"
 
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/saisoma123/BlockPost/x/blockpost/types"
 )
 
@@ -16,7 +19,32 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 	return &msgServer{Keeper: keeper}
 }
 
-func (m msgServer) BlockPostMessage(goCtx context.Context, msg *types.MsgBlockPostMessage) (*types.MsgBlockPostMessageResponse, error) {
+// Calls the addMessage keeper function and validates transactions
+func (m msgServer) BlockPostMessageStore(ctx context.Context, msg *types.MsgBlockPostMessage) (*types.MsgBlockPostMessageResponse, error) {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	// Validates incoming message through creator and message strings
+	if err := msg.ValidateBasic(); err != nil {
+		return nil, err
+	}
+
+	// Adds message to KVStore and checks for any errors in processing
+	messageID, err := m.Keeper.addMessage(sdk.UnwrapSDKContext(ctx), msg.Creator, msg.Message)
+	if err != nil {
+		return nil, errorsmod.Wrapf(sdkerrors.ErrInvalidRequest, "failed to add message")
+	}
+
+	// Emits event for logging history
+	sdkCtx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
+			sdk.NewAttribute(sdk.AttributeKeyAction, "BlockPostMessage"),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.Creator),
+			sdk.NewAttribute("message_id", messageID),
+		),
+	})
+
 	return &types.MsgBlockPostMessageResponse{}, nil
 }
 
